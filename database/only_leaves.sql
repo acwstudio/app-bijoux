28507a302e8493313dd7bc84ff8f82ed72432b52
acwstudio <acwstudio2018@gmail.com>
2024-01-06 13:45:28 +0300

    before refactoring to split jewelleries into jewelleries and usks

---
+++ database/only_leaves.sql
@@ -0,0 +1,4 @@
+select id, name, case when parent_id is null then 'root' 
+when parent_id is not null and id in (select distinct parent_id from catalogs) then 'inner'
+else 'leaf' end as type from catalogs
+having type = 'leaf';
